5b68bd04fec96b18c52559f5cf5586735ceabb40
SaraPubg <62200181+SaraPubg@users.noreply.github.com>
2020-03-15 16:21:34 +0530

    Update extra.py

--- haruka/modules/extra.py
+++ haruka/modules/extra.py
@@ -258,6 +258,7 @@ __help__ = """
  - /bluetext : check urself :V
  - /roll : Roll a dice.
  - /rlg : Join ears,nose,mouth and create an emo ;-;
+ - /lyrics <keyword> : Find your favourite songs' lyrics
 """
 
 __mod_name__ = "Extras"
