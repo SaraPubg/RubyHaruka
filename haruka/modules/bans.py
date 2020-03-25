e9ed172df785dd8ad1d42c3a996ec0ce1b488a92
SaraPubg <62200181+SaraPubg@users.noreply.github.com>
2020-03-25 17:02:09 +0530

    Update bans.py

--- haruka/modules/bans.py
+++ haruka/modules/bans.py
@@ -65,7 +65,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
         keyboard = []
         bot.send_sticker(update.effective_chat.id, BAN_STICKER)  # ban sticker
         reply = "Nub {} Snapped by Pro {} in {}!".format(mention_html(member.user.id, member.user.first_name), mention_html(user.id, user.first_name), html.escape(chat.title))
-        message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.MARKDOWN)
+        message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
         return log
 
     except BadRequest as excp:
