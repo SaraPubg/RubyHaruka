ffd1d20a528817915ac50df079d3081b25f5faa8
SaraPubg <62200181+SaraPubg@users.noreply.github.com>
2020-03-25 13:54:28 +0530

    Update bans.py

--- haruka/modules/bans.py
+++ haruka/modules/bans.py
@@ -71,7 +71,8 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
     except BadRequest as excp:
         if excp.message == "Reply message not found":
             # Do not reply
-            message.reply_text('Nub {} Snapped by Pro {} in {}!'.format(mention_html(member.user.id, member.user.first_name), mention_html(user.id, user.first_name), html.escape(chat.title)), quote=False)
+            reply = "Nub {} Snapped by Pro {} in {}!".format(mention_html(member.user.id, member.user.first_name), mention_html(user.id, user.first_name), html.escape(chat.title)), quote=False)
+            message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
             return log
         else:
             LOGGER.warning(update)
