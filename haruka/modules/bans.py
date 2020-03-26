f5d9e6401e930a3bf4bcb51dc608829b4e44d359
SaraPubg <62200181+SaraPubg@users.noreply.github.com>
2020-03-26 11:05:01 +0530

    Update bans.py

--- haruka/modules/bans.py
+++ haruka/modules/bans.py
@@ -66,16 +66,16 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
         bot.send_sticker(update.effective_chat.id, BAN_STICKER)  # ban sticker
         reply = "{} has been banned!" \
           "\n<b>Reason:</b> {}".format(mention_html(member.user.id, member.user.first_name), (reason))
-        message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.MARKDOWN)
+        message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
 
         return log
 
     except BadRequest as excp:
         if excp.message == "Reply message not found":
             # Do not reply
-            message.reply_text('{} has been banned!' \
-            '\n<b>Reason:</b> {}'.format(mention_html(member.user.id, member.user.first_name), (reason)), quote=False)
-
+            reply = "{} has been banned!" \
+            "\n<b>Reason:</b> {}".format(mention_html(member.user.id, member.user.first_name), (reason))
+             message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
             return log
         else:
             LOGGER.warning(update)
