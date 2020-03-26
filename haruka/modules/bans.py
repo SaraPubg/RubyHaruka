ebe6348341f4730c946b571875d8ecb9ba029c87
SaraPubg <62200181+SaraPubg@users.noreply.github.com>
2020-03-26 10:29:45 +0530

    Update bans.py

--- haruka/modules/bans.py
+++ haruka/modules/bans.py
@@ -64,14 +64,20 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
         chat.kick_member(user_id)
         keyboard = []
         bot.send_sticker(update.effective_chat.id, BAN_STICKER)  # ban sticker
-        reply = "Nub {} Snapped by Pro {} in {}!".format(mention_html(member.user.id, member.user.first_name), mention_html(user.id, user.first_name), html.escape(chat.title))
-        message.reply_text(reply, reply_markup=InlineKeyboardMarkup(keyboard), parse_mode=ParseMode.MARKDOWN)
+        reply = "{} has been banned!".format(mention_html(member.user.id, member.user.first_name))
+        message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
+
+        if reason:
+        log += "\n<b>Reason:</b> {}".format(reason)
         return log
 
     except BadRequest as excp:
         if excp.message == "Reply message not found":
             # Do not reply
-            message.reply_text('Nub {} Snapped by Pro {} in {}!'.format(mention_html(member.user.id, member.user.first_name), mention_html(user.id, user.first_name), html.escape(chat.title)), quote=False)
+            message.reply_text('Banned!', quote=False)
+
+            if reason:
+        log += "\n<b>Reason:</b> {}".format(reason)
             return log
         else:
             LOGGER.warning(update)
