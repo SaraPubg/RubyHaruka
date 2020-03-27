05356937fa5e7c33a1421f209ed91f1bd4544efb
SaraPubg <62200181+SaraPubg@users.noreply.github.com>
2020-03-27 21:26:28 +0530

    Update languages.py

--- haruka/modules/languages.py
+++ haruka/modules/languages.py
@@ -7,7 +7,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, User
 from telegram import Message, Chat, Update, Bot
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, run_async, DispatcherHandlerStop, MessageHandler, Filters, CallbackQueryHandler
-from haruka import dispatcher, spamcheck, LOGGER
+
 from haruka.modules.disable import DisableAbleCommandHandler
 from haruka.modules.helper_funcs.chat_status import user_admin_no_reply, user_admin
 
