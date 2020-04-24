78cd7f6b94a4108d1b56ebecd7a9a5158c0e2a42
SaraPubg <62200181+SaraPubg@users.noreply.github.com>
2020-04-24 16:14:01 +0530

    Update afk.py

--- haruka/modules/afk.py
+++ haruka/modules/afk.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from telegram import Message, Update, Bot, User
-from telegram import MessageEntity, ParseMode
+from telegram import MessageEntity
 from telegram.ext import Filters, MessageHandler, run_async
 
 from haruka import dispatcher
@@ -9,15 +9,12 @@ from haruka.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHa
 from haruka.modules.sql import afk_sql as sql
 from haruka.modules.users import get_user_id
 
-from haruka.modules.translations.strings import tld
-
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
 
 
 @run_async
 def afk(bot: Bot, update: Update):
-    chat = update.effective_chat  # type: Optional[Chat]
     args = update.effective_message.text.split(None, 1)
     if len(args) >= 2:
         reason = args[1]
@@ -25,22 +22,19 @@ def afk(bot: Bot, update: Update):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    fname = update.effective_user.first_name
-    update.effective_message.reply_text(tld(chat.id, f"{fname} is now AFK!"))
+    update.effective_message.reply_text("{} is away from the keyboard ! ".format(update.effective_user.first_name))
 
 
 @run_async
 def no_longer_afk(bot: Bot, update: Update):
     user = update.effective_user  # type: Optional[User]
-    chat = update.effective_chat  # type: Optional[Chat]
 
     if not user:  # ignore channels
         return
 
     res = sql.rm_afk(user.id)
     if res:
-        firstname = update.effective_user.first_name
-        update.effective_message.reply_text(tld(chat.id, f"{firstname} is no longer AFK!"))
+        update.effective_message.reply_text("{} Not far from the keyboard now !".format(update.effective_user.first_name))
 
 
 @run_async
@@ -64,30 +58,20 @@ def reply_afk(bot: Bot, update: Update):
             else:
                 return
 
-            check_afk(bot, update, user_id, fst_name)
-
-    elif message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        fst_name = message.reply_to_message.from_user.first_name
-        check_afk(bot, update, user_id, fst_name)
-
-
-def check_afk(bot, update, user_id, fst_name):
-    chat = update.effective_chat  # type: Optional[Chat]
-    if sql.is_afk(user_id):
-        user = sql.check_afk_status(user_id)
-        if not user.reason:
-            res = tld(chat.id, f"{fst_name} is AFK!")
-        else:
-            res = tld(chat.id, f"{fst_name} is AFK! says its because of:\n{user.reason}")
-        update.effective_message.reply_text(res)
+            if sql.is_afk(user_id):
+                user = sql.check_afk_status(user_id)
+                if not user.reason:
+                    res = "{} is away from the keyboard ! reason :\n{} ".format(fst_name)
+                else:
+                    res = "{} is away from the keyboard ! reason :\n{}. ".format(fst_name, user.reason)
+                message.reply_text(res)
 
 
 __help__ = """
  - /afk <reason>: mark yourself as AFK.
  - brb <reason>: same as the afk command - but not a command.
 
-When marked as AFK, any mentions will be replied to with a message to say that you're not available!
+When marked as AFK, any mentions will be replied to with a message to say you're not available!
 """
 
 __mod_name__ = "AFK"
@@ -95,7 +79,8 @@ __mod_name__ = "AFK"
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
 AFK_REGEX_HANDLER = DisableAbleRegexHandler("(?i)brb", afk, friendly="afk")
 NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
-AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group, reply_afk)
+AFK_REPLY_HANDLER = MessageHandler(Filters.entity(MessageEntity.MENTION) | Filters.entity(MessageEntity.TEXT_MENTION),
+                                   reply_afk)
 
 dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
